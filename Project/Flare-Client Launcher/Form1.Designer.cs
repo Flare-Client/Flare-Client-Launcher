b88387ecbb8b9181facb923770e0c51066a3eb10
DisabledMallis <mallisiscool@gmail.com>
2020-01-19 01:58:13 -0500

    Extract zip

---
+++ Project/Flare-Client Launcher/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Flare_Client_Launcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.button1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.button1.Location = new System.Drawing.Point(287, 179);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(211, 108);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Download and Launch";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(305, 302);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(170, 23);
+            this.progressBar.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GrayText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Flare-Client Launcher";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar;
+    }
+}
+
---
+++ Project/Flare-Client Launcher/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Diagnostics;
+using System.IO;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Flare_Client_Launcher
+{
+    public partial class Form1 : Form
+    {
+        WebClient Client = new WebClient();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Client.DownloadFileCompleted += new AsyncCompletedEventHandler(FileDownloadComplete);
+            Uri DownloadsURL = new Uri("https://github.com/Flare-Client/Flare-Client/releases/latest/download/Flare_Client.exe");
+            Client.DownloadFileAsync(DownloadsURL, "Flare_Client.exe");
+            Client.DownloadProgressChanged += (s, o) =>
+            {
+                progressBar.Value = o.ProgressPercentage;
+            };
+        }
+        private void FileDownloadComplete(object sender, AsyncCompletedEventArgs e)
+        {
+            MessageBox.Show("Finished Downloading Latest Release...");
+            string currDir = Directory.GetCurrentDirectory();
+            if(File.Exists(currDir + "/Flare_Client.exe"))
+            {
+                Process.Start(currDir + "/Flare_Client.exe");
+            } else
+            {
+                MessageBox.Show("Unable to Launch Flare-Client, do you have a valid internet connection?");
+            }
+        }
+    }
+}
